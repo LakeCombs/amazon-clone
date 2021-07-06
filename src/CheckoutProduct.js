cdf4b861315f88866ba46d17ebd0f82832baa689
LakeCombs <adesolaolamilekan2@gmailcom>
2021-07-06 12:56:33 +0100

    fix button

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./CheckoutProduct.css";
 import { useStateValue } from "./StateProvider";
 
-function CheckoutProduct({ id, image, title, price, rating }) {
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
   const [{ basket }, dispatch] = useStateValue();
 
   const removeFromBasket = () => {
@@ -15,7 +15,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
 
   return (
     <div className="checkoutProduct">
-      <img src={image} className="checkoutProduct__image" />
+      <img src={image} className="checkoutProduct__image" alt="" />
       <div className="checkoutProduct__info">
         <p className="checkoutProduct__title">{title}</p>
         <p className="checkoutProduct__price">
@@ -29,13 +29,15 @@ function CheckoutProduct({ id, image, title, price, rating }) {
               <p>⭐</p>
             ))}
         </div>
-        <button
-          onClick={() => {
-            removeFromBasket();
-          }}
-        >
-          Remove from Basket{" "}
-        </button>
+        {!hideButton && (
+          <button
+            onClick={() => {
+              removeFromBasket();
+            }}
+          >
+            Remove from Basket{" "}
+          </button>
+        )}
       </div>
     </div>
   );
